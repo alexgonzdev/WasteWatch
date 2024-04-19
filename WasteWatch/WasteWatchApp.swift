8eb69fee878bc5579682180212663b288d9c0c60
Alex Gonzalez <alexgonzdev@gmail.com>
2024-04-19 15:43:18 -0400

    Initial Commit

---
+++ WasteWatch/WasteWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WasteWatchApp.swift
+//  WasteWatch
+//
+//  Created by Alex Gonzalez on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WasteWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
